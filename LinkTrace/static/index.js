bffa7b8804c554bcb46e51cef631a36703c85de1
aadityajoshi151 <aadityajoshi151@gmail.com>
2021-04-21 22:59:25 +0530

    loader changed

--- LinkTrace/static/index.js
+++ LinkTrace/static/index.js
@@ -1,4 +1,3 @@
 document.getElementById("searchbtn").addEventListener("click",function(){
     document.getElementById("loader").style.visibility = "visible";
 })
-setTimeout(function(){ alert("Hello"); }, 60000);
